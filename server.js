a1d2a38f1aaf28480a33fd718ac6e130e257f840
mirlan <programchy95@gmail.com>
2018-05-13 20:00:47 +0600

    1commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mc.connect();
 
 // default route
 app.get('/', function (req, res) {
-    return res.send({ error: true, message: 'hello',miki:'s' })
+    return res.send({ error: true, message: 'hello' })
 });
  //get themes from tester_exams table
  app.get('/themes', function(req,res){
@@ -29,46 +29,66 @@ app.get('/', function (req, res) {
  		return res.send({error : false ,data: results, message : 'themes list'});
  	});
  });
- app.get('/ques', function(req, res){
- 	var username = req.params.username;
- 	var password = req.params.password;
- 	var exam_id = req.params.exam_id;	
- 	
- 	console.log(req.body.password);
- 		console.log(req.params.username);
+ app.post('/ques', function(req, res){
+ 	var username = req.body.username;
+ 	var password = req.body.password;
+ 	var exam_id = req.body.exam_id;	
+ 	var student_id;
+
  	if(!exam_id || !username || !password){    
- 		
- 		return res.status(400).send({ error:true, message: 'Please prjasdbfovide tas'});
+ 		return res.status(400).send({ error:true, message: 'error'});
     }
 
     mc.query('select student_id from tester_students where number=?',username,function(error,result,fields){
-    	if(error)throw error;
-    	var student_id = result[0].student_id;
+    	if(error)return res.status(400).send({ error:true, message: 'error'});;
+    	 student_id = result[0].student_id;
+    	
     });
 
  	mc.query('SELECT password from tester_exampasswords where exam_id = ? AND student_id = (SELECT student_id from tester_students where number= ?)', [exam_id,username], function(error, results, fields){
- 		if(error)throw error;
- 		console.log(results[0].password);
- 		console.log(password);
- 		if(results[0].password==password){
+ 		if(error)return res.status(400).send({ error:true, message: 'error'});;
+ 		
+ 		if(results!=0 && results[0].password==password){
+ 		
  			mc.query('select result from tester_examresults where exam_id = ? AND student_id = ?',[exam_id,student_id],function(error,result,fields){
- 				if(error)throw error;
- 				if(!result[0].result){
- 					return res.send({success:false});
+ 				if(error)return res.status(400).send({ error:true, message: 'error'});;
+ 				
+ 				if(result[0].result){//for testing <= if(!result[0].result)
+                   // console.log(result[0].result);
+                    var themes = [];
+                    mc.query('select theme_id, simple_count, normal_count, hard_count from tester_examthemes where exam_id  = ? ', exam_id,function(error, result, fields) {
+                            if (error) return res.status(400).send({error: true, message: error});
+                            var query = "";
+                            var attrs =  [];
+                            for (var i = 0; i < result.length; i++) {
+                                //console.log(result);
+                                var levels = [result[i].simple_count, result[i].normal_count, result[i].hard_count]
+                                for (var j = 0; j < 3; j++){
+                                    if (i > 0 || j > 0) query += " UNION";
+                                    query += " (SELECT * FROM tester_questions WHERE theme_id = ? AND level = " + (j + 1) + " ORDER BY RAND() LIMIT ?)";
+                                    attrs.push(result[i].theme_id)
+                                    attrs.push(levels[j])
+                                }
+                            }
+                            mc.query(query, attrs, function(error, result, fields){
+                                if (error) return res.status(400).send({error: true, message: error});
+                                //console.log(fields);
+                                //console.log(result);
+                                return res.send({result});
+                            });
+                        }
+                        );
+ 				}else {
+ 					return res.send({error:true,message: 'you are finished'});
  				}
- 				else {
- 					return res.send({miki:mik});
- 				}
+
  			});
  		}
- 		else return res.send({miki:results});
+ 		
  	});
-
- 	//var password = mc.query('select password from tester_exampasswords where student_id = ?', student_id);
- 	//console.log(password);
- 	//var result = mc.query('select result from tester_examresults where exam_id = ? && student_id = ?'
  	
  });
+
 // port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
 app.listen(8080, function () {
     console.log('Node app is running on port 8080');
